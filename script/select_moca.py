5f63c99d8ef60052b66a4a2d5177d2d0d2b1e3d4
vbanimoca <122361037+vbanimoca@users.noreply.github.com>
2023-04-09 20:14:09 +0800

    refactor

--- script/select_moca.py
+++ script/select_moca.py
@@ -26,88 +26,72 @@ num_winners = args.num_winners
 wl_name = args.whitelist_name
 max_wins_per_wallet = args.max_wins_per_wallet
 
-# # TODO fix case if stake_at_week is last week, calculate the week number from timestamp
-# stake_at_week = args.stake_at_week
+# TODO fix case if stake_at_week is last week, calculate the week number from timestamp
+stake_at_week = args.stake_at_week
 
 # TODO add function call to get block number and block hash with timestamp
 # # Define the blockhash for the randomization
 # # blocknumber: etherscan.io/block/16947336
 # blockhash = args.blockhash
 
-
 blockhash = '0xaf45a04a30cf7513398dfc2e60cb0e2d022dd8b292de4b5990dcc6a323e7697f'
 
+# Define the seed phrase for the randomization
+seed_phrase = wl_name + blockhash
 
 # Read the CSV file and create a list of dictionaries
 with open(args.csv_file, 'r') as f:
     reader = csv.DictReader(f)
     data = [row for row in reader]
 
-# Initialize the wallets dictionary
-wallets = {}
-
 with open('data/moca_name_mapping.json', 'r') as f:
     moca_mapping = json.load(f)
 
-# Loop through each row in the data and add it to the wallets dictionary
+# Initialize the wallets dictionary and the list of eligible Mocas
+wallets = {}
+eligible_mocas = []
+
+# Loop through each row in the data
 for row in data:
     moca_id = int(row['moca_id'])
     wallet = row['wallet']
     moca_xp = int(row['moca_xp'])
-    #week = int(row['week'])
+    week = int(row['week'])
     moca_name = next((item['moca_name']
                      for item in moca_mapping if item['token_id'] == str(moca_id)), None)
-    if wallet not in wallets:
-        wallets[wallet] = []
-    wallets[wallet].append({'id': moca_id, 'xp': moca_xp, 'name': moca_name})
 
+    wallets.setdefault(wallet, []).append({'id': moca_id, 'xp': moca_xp, 'name': moca_name, 'week': week})
+
+    # Check if the Moca has enough XP to be eligible
+    if week == stake_at_week:
+        eligible_mocas.append(
+            {'id': moca_id, 'xp': moca_xp, 'name': moca_name, 'week': week})
 
 # Convert the wallets dictionary to a list of dictionaries with 'address' and 'mocas' keys
 wallets = [{'address': wallet, 'mocas': mocas}
            for wallet, mocas in wallets.items()]
 
-
-# Check for duplicate wallets
-if len(set(wallet['address'] for wallet in wallets)) != len(wallets):
-    raise ValueError('Duplicate wallets found')
-
-# Check for duplicate Moca IDs
+# Check for duplicate wallets and Moca IDs
 moca_ids = set()
 for wallet in wallets:
+    if wallet['address'] in moca_ids:
+        raise ValueError(f'Duplicate wallet address {wallet["address"]} found')
+    moca_ids.add(wallet['address'])
+
     for moca in wallet['mocas']:
         if moca['id'] in moca_ids:
             raise ValueError(f'Duplicate Moca ID {moca["id"]} found')
         moca_ids.add(moca['id'])
 
-# Sort the wallets list by address in alphabetical order
-wallets = sorted(wallets, key=lambda wallet: wallet['address'])
-
-# Define the seed phrase for the randomization
-seed_phrase = wl_name + blockhash
-
-# Set the seed for the randomization
-random.seed(seed_phrase)
-
 # Initialize the winners list and the wallet wins dictionary
 winners = []
 wallet_wins = {wallet['address']: 0 for wallet in wallets}
 
-# Initialize the list of eligible Mocas
-eligible_mocas = []
+# Sort the wallets list by address in alphabetical order
+wallets = sorted(wallets, key=lambda wallet: wallet['address'])
 
-
-# Loop through each wallet and their Mocas
-for wallet in wallets:
-
-    # Get the list of Mocas for this wallet
-    mocas = wallet['mocas']
-
-    # # Remove Mocas that have XP below the threshold
-    # mocas = [moca for moca in mocas if moca['week'] == stake_at_week]
-
-    # Add the remaining Mocas to the list of eligible Mocas
-    eligible_mocas.extend(mocas)
-
+# Set the seed for the randomization
+random.seed(seed_phrase)
 
 # Loop until we have the required number of winners
 while len(winners) < num_winners:
@@ -160,29 +144,17 @@ while len(winners) < num_winners:
     winners.append({'moca_id': moca['id'], 'address': address})
     wallet_wins[address] += 1
 
-# with open('data/premint_wallets.json', 'r') as f:
-    # premint_wallets_ls = json.load(f)
-
-# Create a dictionary that maps each Moca ID to its premint wallet address
-# moca_id_to_wallet = {moca['token_id']: moca['mint_wallet_address'] for moca in premint_wallets_ls}
-
 # Print the list of winners
 for i, winner in enumerate(winners):
     print(
         f'Winner #{i+1}: Moca #{winner["moca_id"]} owned by {winner["address"]}')
     moca_id = winner['moca_id']
-    # premint_wallet = moca_id_to_wallet.get(moca_id) # get the premint wallet address for the Moca ID
-    # if premint_wallet:
-    #     winners[i]['address'] = premint_wallet # replace the 'address' value with the premint wallet address
-    # print(
-    #     f'Winner #{i+1}: Moca #{winner["moca_id"]} owned by PREMINT -  {winner["address"]}')
     moca_name = moca_mapping[moca_id]['moca_name']
     winner['moca_name'] = moca_name
-
 
 # Define timestamp as a string containing the current date and time
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Generate the HTML files
-generate_html(wl_name, wallets, max_wins_per_wallet,
+generate_html(wl_name, wallets, max_wins_per_wallet, stake_at_week,
               num_winners, seed_phrase, winners, timestamp)
